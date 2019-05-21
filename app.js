01d996867e1f417b11e5c543f14427cc5ea6190b
Lianginger <jackyliang444@hotmail.com.tw>
2019-05-21 14:34:30 +0800

    Connect to monboDB

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/urlShortener', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+  console.log('MongoDB id connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
