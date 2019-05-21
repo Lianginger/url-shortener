4b4af64f2ecfeb5225151220c88030ee925bfb8e
Lianginger <jackyliang444@hotmail.com.tw>
2019-05-21 14:42:00 +0800

    Add UrlShorten model

---
+++ models/url-shorten.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+var urlShortenSchema = new mongoose.Schema({
+  originalUrl: String,
+  shortId: String,
+  createdAt: String
+})
+
+module.exports = mongoose.model('UrlShorten', urlShortenSchema);
