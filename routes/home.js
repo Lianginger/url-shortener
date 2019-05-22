8df6a7dccdc6bc582b63c7c85fe6a123d80f3401
Lianginger <jackyliang444@hotmail.com.tw>
2019-05-22 14:02:13 +0800

    Add url validation

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const UrlShorten = require('../models/url-shorten')
 const moment = require('moment')
 const tz = require('moment-timezone')
+const dns = require('dns')
 
 router.get('/', (req, res) => {
   res.render('home')
@@ -11,22 +12,32 @@ router.get('/', (req, res) => {
 router.post('/new', async (req, res) => {
   const baseUrl = `${req.protocol}://${req.headers.host}/`
   const newOriginalUrl = req.body.originalUrl
-  //驗證URL是否為真
 
-  //存入DB
-  //是否為重複的URL，如果是，直接給過去的 shortId
-  const exitShortId = await findOriginalUrlInDataBaseAndReturnShortId(newOriginalUrl)
-  if (exitShortId) {
-    res.render('home', { baseUrl, exitShortId })
-  } else {
-    const newShortId = await generateUniqueShortId()
-    const newUrlShorten = new UrlShorten({
-      originalUrl: newOriginalUrl,
-      shortId: newShortId,
-      createdAt: moment().tz('Asia/Taipei').format('YYYY-MM-DD')
-    })
-    await newUrlShorten.save()
-    res.render('home', { baseUrl, newShortId })
+  try {
+    //驗證URL
+    let validUrl
+    validUrl = new URL(newOriginalUrl)
+    await isSiteExit(validUrl)
+
+    //存入DB
+    //是否為重複的URL，如果是，直接給過去的 shortId
+    const exitShortId = await findOriginalUrlInDataBaseAndReturnShortId(newOriginalUrl)
+    if (exitShortId) {
+      res.render('home', { baseUrl, exitShortId })
+    } else {
+      const newShortId = await generateUniqueShortId()
+      const newUrlShorten = new UrlShorten({
+        originalUrl: newOriginalUrl,
+        shortId: newShortId,
+        createdAt: moment().tz('Asia/Taipei').format('YYYY-MM-DD')
+      })
+      await newUrlShorten.save()
+      res.render('home', { baseUrl, newShortId })
+    }
+
+  } catch (err) {
+    const errorMessage = '錯誤的網址！請確定網址是否正確'
+    res.render('home', { errorMessage, newOriginalUrl })
   }
 })
 
@@ -42,6 +53,17 @@ router.get('/:shortId', async (req, res) => {
 })
 
 module.exports = router
+
+function isSiteExit(validUrl) {
+  return new Promise((resolve, reject) => {
+    dns.lookup(validUrl.hostname, err => {
+      if (err) {
+        return reject(false)
+      }
+      return resolve(true)
+    })
+  })
+}
 
 async function findOriginalUrlByShortIdAndReturnExitOriginalUrl(shortId) {
   const urlShorten = await UrlShorten.findOne({ shortId }).exec()
